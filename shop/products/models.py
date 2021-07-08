02ae9fe6d4700bfef076dc3cf1c4754016497e6b
onionj <onionj98@gmail.com>
2021-07-08 23:33:14 +0430

    15- add product to admin

--- shop/products/models.py
+++ shop/products/models.py
@@ -8,3 +8,6 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(max_digits=20, decimal_places=2, default=0.0)
+
+    def __str__(self) -> str:
+        return self.title
