242128fe4effcf1e02e9ecfced2d7f6b71dd0064
onionj <onionj98@gmail.com>
2021-07-11 20:00:25 +0430

    20- 404 error handeling in product_list_view

--- shop/products/views.py
+++ shop/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
+from django.http import Http404
 # Create your views here.
 
 from .models import Product
@@ -20,7 +21,7 @@ class ProductListView(ListView):
 def product_list_view(requests):
     products = Product.objects.all()
     context = {
-        'qr': products
+        'object_list': products
     }
 
     return render(requests, "products/product_list.html", context)
@@ -38,8 +39,21 @@ class ProductDetailView(DetailView):
 
 
 def product_detail_view(request, productId=None, *args, **kwargs):
-    # product = Product.objects.get(id=productId)
-    product = get_object_or_404(Product, id=productId)
+    # try:
+    #     product = Product.objects.get(id=productId)
+    # except Product.DoesNotExist:
+    #     raise Http404("product does not found from try except")
+    # except:
+    #     print("what?")
+
+    qs = Product.objects.filter(id=productId)
+    # print(qs)
+    if qs.exists() and qs.count() == 1:
+        product = qs.first()
+    else:
+        raise Http404("product does not found _______")
+
+    # product = get_object_or_404(Product, id=productId)
     context = {
         "object": product
     }
